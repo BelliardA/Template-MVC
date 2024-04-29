9a6140099f40323eab1a37d64eac84db42148afb
Arnaud <arnaud.belliard@crea-inseec.com>
2024-04-29 12:02:13 +0200

    creation controller et bdd

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -2,7 +2,7 @@
 
 function controleurPrincipal($action) {
     $lesActions = array();
-    // $lesActions["defaut"] = "exemple.php ";     // enter the name of the default controller
+    $lesActions["defaut"] = "user.php ";     // enter the name of the default controller
 
 
     if (array_key_exists($action, $lesActions)) {
